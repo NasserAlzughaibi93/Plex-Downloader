483cf40ec92e89a3e37f4fb77c7316fb4d31aa26
naz <n.zughaibi@gmail.com>
2020-01-05 09:12:28 +0300

    PLEXDL-2 Ability to Login through Plex OAuth
    Added OAuth Sign in. Minor refactoring and UI stuff.

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LoginService} from '../_service/login.service';
+import {Constants} from "../util/constants";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -10,16 +12,60 @@ export class LoginComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private loginService: LoginService) { }
+  isBasicLogin = false;
 
-  ngOnInit() {
-  }
+  oAuthPinTimer: any;
 
-  login() {
+  constructor(private loginService: LoginService,
+              private router: Router,) {}
+
+  ngOnInit() {}
+
+  basicLogin() {
     this.loginService.login(this.model).subscribe(() => {
       console.log('worked');
+      this.router.navigate(['/home']);
     }, () => {
       console.log('Error');
     });
+  }
+
+  oAuthLogin() {
+    const oAuthWindow = window.open(window.location.toString(), "_blank", `toolbar=0,
+        location=0,
+        status=0,
+        menubar=0,
+        scrollbars=1,
+        resizable=1,
+        width=500,
+        height=500`);
+
+    this.loginService.retrieveOAuthLoginPinAndUrl().subscribe(plexPin => {
+      console.log("Retrieved Pin: " + plexPin.id);
+      console.log("Retrieved resolved URI: " + plexPin.resolvedUri);
+      oAuthWindow!.location.replace(plexPin.resolvedUri);
+
+      this.oAuthPinTimer = setInterval(() => {
+        //this.notify.info("Authenticating", "Loading... Please Wait");
+        //this.getPinResult(x.pinId);
+        this.loginService.retrieveOAuthPinResults(plexPin.id).subscribe(pin => {
+          if (pin.authToken) {
+            localStorage.setItem(Constants.PLEX_AUTH_TOKEN, pin.authToken);
+            this.ngOnDestroy();
+            this.router.navigate(['/home']);
+          }
+        }, error => {
+          console.log("Error: " + error);
+          //TODO route back to login
+        })
+      }, 10000);
+
+    }, error => {
+      console.log("Error: " + error);
+    });
+  }
+
+  public ngOnDestroy() {
+    clearInterval(this.oAuthPinTimer);
   }
 }
