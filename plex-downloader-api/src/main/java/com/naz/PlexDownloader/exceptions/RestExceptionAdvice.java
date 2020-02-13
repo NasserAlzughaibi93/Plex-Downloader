84248af85967cf4fd61ad519fd7debd9ec4c0559
naz <n.zughaibi@gmail.com>
2020-02-13 12:17:13 -0500

    JWT Test Implementation
    Added exception handling

--- plex-downloader-api/src/main/java/com/naz/PlexDownloader/exceptions/RestExceptionAdvice.java
+++ plex-downloader-api/src/main/java/com/naz/PlexDownloader/exceptions/RestExceptionAdvice.java
@@ -1,11 +1,14 @@
 package com.naz.PlexDownloader.exceptions;
 
+import com.naz.PlexDownloader.exceptions.rest.JwtInvalidException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -19,6 +22,17 @@ public class RestExceptionAdvice extends ResponseEntityExceptionHandler {
         String bodyOfResponse = ex.getMessage();
         return handleExceptionInternal(ex, bodyOfResponse,
                 new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+    //TODO - global error handling doesn't work.
+    @ExceptionHandler(value
+            = { JwtInvalidException.class, BaseRuntimeException.class})
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    protected ResponseEntity<Object> handleInvalidJwtToken(
+            RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
     }
 
 }
